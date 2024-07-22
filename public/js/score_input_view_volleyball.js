2911983da72008932594cd53f15f201843533fdf
bkmistry1 <bmistry1@stevens.edu>
2024-07-22 14:37:36 -0400

    -added team names functionality -added manual score input functionality

--- public/js/score_input_view_volleyball.js
+++ public/js/score_input_view_volleyball.js
@@ -6,6 +6,11 @@
 
 	const score1 = $('#score1')
 	const score2 = $('#score2')
+	const team1 = $('#team1')
+	const team2 = $('#team2')
+	const set1 = $('#set1')
+	const set2 = $('#set2')
+
 
 	$(":button").click(function (event) {
 		event.preventDefault()
@@ -33,7 +38,7 @@
 		scoreObj.team1 = score1.val()
 		scoreObj.team2 = score2.val()
 
-		socket.emit(socketStringIdentifier, scoreObj)
+		sendScoreToSocket()
 
 	});	
 
@@ -48,7 +53,49 @@
 		scoreObj.team1 = 0
 		scoreObj.team2 = 0
 
-		socket.emit(socketStringIdentifier, scoreObj)
+		sendScoreToSocket()
 	})
+
+
+	$("#set_score_button").click(function (event) {
+		event.preventDefault()
+
+		const scoreTeam1 = $('#set_input_1')
+		const scoreTeam2 = $('#set_input_2')
+
+		score1.val(scoreTeam1.val())
+		score2.val(scoreTeam2.val())
+
+		sendScoreToSocket()
+
+	});		
+
+	$("#set_team_button").click(function (event) {
+		event.preventDefault()
+
+		const teamName1 = $('#team_input_1').val()
+		const teamName2 = $('#team_input_2').val()
+
+		team1.val(teamName1)
+		team2.val(teamName2)
+
+		sendScoreToSocket()
+
+	});			
+
+	function sendScoreToSocket() {
+		let scoreObj = {}
+
+		scoreObj.team1 = score1.val()
+		scoreObj.team2 = score2.val()
+
+		scoreObj.teamName1 = team1.val()
+		scoreObj.teamName2 = team2.val()
+
+		scoreObj.set1 = set1.val()
+		scoreObj.set2 = set2.val()
+
+		socket.emit(socketStringIdentifier, scoreObj)
+	}
 
 })(window.jQuery)
--- public/js/scoreboard_view_volleyball.js
+++ public/js/scoreboard_view_volleyball.js
@@ -6,8 +6,21 @@
 	socket.on('score_volleyball', scoreObj => {
 		let score1 = $('#score1')
 		let score2 = $('#score2')
+		
+		let team1 = $('#team1')
+		let team2 = $('#team2')		
+
+		let set1 = $('#set1')
+		let set2 = $('#set2')
 
 		score1.val(scoreObj.team1)
 		score2.val(scoreObj.team2)
+
+		team1.val(scoreObj.teamName1)
+		team2.val(scoreObj.teamName2)		
+
+		set1.val(scoreObj.set1)
+		set2.val(scoreObj.set2)				
+
 	})
 })(window.jQuery)
