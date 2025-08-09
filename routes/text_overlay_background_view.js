0c7042e20a1967282d1c2c107c16c0c635b48ffb
bkmistry1 <bmistry1@stevens.edu>
2025-08-09 01:36:16 -0400

    -add text overlay styling -adding input field to display text

--- routes/text_overlay_background_view.js
+++ routes/text_overlay_background_view.js
@@ -5,7 +5,7 @@ router.get('/', async (req, res) => {
 	try {
 		return res.render('partials/text_overlay_background', {
 			title: 'Text Overlay Background',
-			shortcode: 'text_overlay_background_style',
+			shortcode: 'text_overlay_style',
 		})
 	} catch (e) {
 		return res.status(500).json({ error: e })
